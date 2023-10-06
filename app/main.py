08881070def1c9fad96c1c225b7460dfec049621
Federico <sambu00@gmail.com>
2023-10-06 22:21:57 +0200

    jwt on positive response

--- app/main.py
+++ app/main.py
@@ -1,22 +1,25 @@
+import os
+from datetime import datetime, timedelta
+
+from credential import Credential
+from user_status import UserStatus
+
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 import couchdb
-
-from credential import Credential
-
-from user_status import UserStatus
-
-import os
+import jwt
 
 
 DB_USER = 'admin'
 DB_PASSWORD = 'pwd'
 DB_ADDRESS = "localhost:5984"
 
+JWT_SECRET = 'my_dirty_secret'
+
+
 class CredentialWrapper(BaseModel):
     user: str
     secret: str
-
 
 
 app = FastAPI()
@@ -35,7 +38,11 @@ def validate_user(credential_wrap: CredentialWrapper, response: Response):
 
     resp = {}
     if valid_credential(credential):
-        resp = {"status": "FOUND"} #crea JWT
+        jwt_payload = {'iat' : datetime.now(),
+                       'exp' : datetime.now() + timedelta(minutes=15),
+                       'user' : credential.user}
+        resp = jwt.encode(jwt_payload, JWT_SECRET, "HS256")
+        
     else:
         response.status_code = status.HTTP_400_BAD_REQUEST
         resp = {"msg": "wrong user or password"}
